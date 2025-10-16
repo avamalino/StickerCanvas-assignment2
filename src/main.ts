66c3b528049460fb3e2a7ddb7fb4852668eeabb2
Ava :3 <138967687+avamalino@users.noreply.github.com>
2025-10-16 20:36:47 +0000

    set up a canvas for the page as well as added in the css to add a black border around it. And you can stamp squares for funsies. Step 1 complete
    Co-authored-by: Kaitlyn Eng <kaitlyn-png@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,26 @@ import "./style.css";
 
 document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <h1> Herro <h1>
+  <canvas id="cvs"></canvas>
 `;
+
+const canvas = document.getElementById("cvs") as HTMLCanvasElement;
+canvas.id = "cvs";
+const context = canvas.getContext("2d");
+
+//if (context) {
+canvas.addEventListener("mousedown", (event) => {
+  const rect = canvas.getBoundingClientRect();
+  const x = event.clientX - rect.left;
+  const y = event.clientY - rect.top;
+  const gradient = context?.createLinearGradient(0, 0, 200, 0);
+  gradient?.addColorStop(0, "pink");
+  gradient?.addColorStop(1, "purple");
+
+  if (context) {
+    context.fillStyle = gradient!;
+    context.fillRect(x, y, 20, 20);
+  }
+});
+//}
