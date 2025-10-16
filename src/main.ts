6326b9c9b2e8ce637d100c6b30a55295063eaf08
Ava :3 <138967687+avamalino@users.noreply.github.com>
2025-10-16 21:00:13 +0000

    added drawing on canvas by following mouse movement. also added clear button. step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,47 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
+//import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
   <h1> Herro <h1>
   <canvas id="cvs"></canvas>
 `;
 
 const canvas = document.getElementById("cvs") as HTMLCanvasElement;
 canvas.id = "cvs";
-const context = canvas.getContext("2d");
+const context = canvas.getContext("2d")!;
+const gradient = context.createLinearGradient(0, 0, 200, 0);
+gradient.addColorStop(0, "pink");
+gradient.addColorStop(1, "purple");
 
-//if (context) {
+const cursor = { active: false, x: 0, y: 0 };
+
 canvas.addEventListener("mousedown", (event) => {
-  const rect = canvas.getBoundingClientRect();
-  const x = event.clientX - rect.left;
-  const y = event.clientY - rect.top;
-  const gradient = context?.createLinearGradient(0, 0, 200, 0);
-  gradient?.addColorStop(0, "pink");
-  gradient?.addColorStop(1, "purple");
+  cursor.active = true;
+  cursor.x = event.offsetX;
+  cursor.y = event.offsetY;
+});
 
-  if (context) {
-    context.fillStyle = gradient!;
-    context.fillRect(x, y, 20, 20);
+canvas.addEventListener("mousemove", (event) => {
+  if (cursor.active) {
+    context.fillStyle = gradient; //ill figure out later
+    context.beginPath();
+
+    context.moveTo(cursor.x, cursor.y);
+    context.lineTo(event.offsetX, event.offsetY);
+    context.stroke();
+    cursor.x = event.offsetX;
+    cursor.y = event.offsetY;
   }
 });
-//}
+
+canvas.addEventListener("mouseup", () => {
+  cursor.active = false;
+});
+
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "clear";
+document.body.append(clearButton);
+
+clearButton.addEventListener("click", () => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+});
